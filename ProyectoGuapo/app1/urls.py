13642fe41ba24f39eef78e37117e311fa8a31b4c
David <David>
2022-02-09 16:36:21 +0100

    a

--- ProyectoGuapo/app1/urls.py
+++ ProyectoGuapo/app1/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from django.conf.urls import patterns, include, url
 
 from . import views
 
 app_name = "app1"
-urlpatterns = [
-    path("", views.index, name="index")
-]
+urlpatterns = patterns(
+    url("", views.index)
+)
--- ProyectoGuapo/app1/views.py
+++ ProyectoGuapo/app1/views.py
@@ -4,3 +4,9 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'index.html')
+
+def search(request):
+    if request.method == "POST":
+        data = request.POST.get("textfield", None)
+        html = ("<H1>%s</H1>", data)
+        return HttpResponse(html)
