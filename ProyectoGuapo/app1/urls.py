37519e39309e8419f016f9564357741db5b76849
David Bellon <dadbc16@gmail.com>
2022-03-17 23:32:36 +0100

    a

--- ProyectoGuapo/app1/urls.py
+++ ProyectoGuapo/app1/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 app_name = "app1"
 urlpatterns = [
-    path("/", views.index, name="index"),
+    path("", views.index, name="index"),
     path("forum/", views.render_forum),
     path("forum/search/", views.search, name="search"),
     path("sign_up/", views.sign_up , name="Nose")
