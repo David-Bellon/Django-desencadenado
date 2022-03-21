13249d8d161ec3365cacd76513d4b0679283b835
David Bellon <dadbc16@gmail.com>
2022-03-22 00:50:48 +0100

    soy un genio

--- ProyectoGuapo/app1/views.py
+++ ProyectoGuapo/app1/views.py
@@ -1,40 +1,47 @@
+from urllib import response
 from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.urls import reverse
 from .models import Usuarios
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html')
-
-def search(request):
-    if request.method == "POST":
-        data = request.POST.get("textfield", None)                       
+    context = {}
+    if request.COOKIES.get("name") is None:
         context = {
-            "texto": data
+            "picture": "static\images\profie-none.svg",
+            "loged": False
         }
-        return render(request, "hola.html", context)
     else:
-        return render(request, 'hola.html')
+        #if cookie exists take the user name, followers, total likes
+        #Look in the data base the profile picture and put in context
+        #context = {
+        #    "profie-picture": "path to the picture in the db"
+        #    "loged": True
+        #    "user_name": -----
+        #    "followers": -----
+        #    "total_ikes": -----
+        #}
+        None
+
+    return render(request, 'index.html', context=context)
 
 def sign_up(request):
     if request.method == "POST":
         user = request.POST.get("textfield")
         email = request.POST.get("emailfield")
         password = request.POST.get("passwordfield")
-        
-        context = {
-            "name": user,
-            "email": email,
-            "pass": password,
-            "meta": Usuarios.objects.all()
-        }
+
+        context = {}
+
         if str(user) == "" or str(email) == "" or str(password) == "":
             context["error"] = "Algunos de los campos esta vacio"
             return render(request, "signup.html", context)
         else:
             new_user = Usuarios.objects.create(name= user, password= password, email= email)
             new_user.save()
-            response = render(request, "signup.html", context)
+            #response = render(request, 'index.html')
+            response = HttpResponseRedirect("/")
             response.set_cookie('name', str(user), max_age=600)
             return response
         
@@ -43,7 +50,3 @@ def sign_up(request):
                 "meta": Usuarios.objects.all()
             }
         return render(request, "signup.html", context=context)
-    
-
-def render_forum(request):
-    return render(request, "form.html")
