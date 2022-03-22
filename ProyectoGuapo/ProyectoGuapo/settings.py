41179578b3b6df036c92c64951f572225964a658
David Bellon <dadbc16@gmail.com>
2022-03-22 19:58:37 +0100

    a

--- ProyectoGuapo/ProyectoGuapo/settings.py
+++ ProyectoGuapo/ProyectoGuapo/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -88,7 +89,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
@@ -133,3 +133,5 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 django_heroku.settings(locals())
+
+#DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
