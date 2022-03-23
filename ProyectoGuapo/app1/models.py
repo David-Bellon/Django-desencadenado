debce0f99c88e991fa6d9c316dd2b4022207995a
David Bellon <dadbc16@gmail.com>
2022-03-23 12:25:49 +0100

    a

--- ProyectoGuapo/app1/models.py
+++ ProyectoGuapo/app1/models.py
@@ -3,15 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Usuarios(models.Model):
-    followers = models.IntegerField(null=False)
-    total_likes = models.IntegerField(null=False)
+    #followers = models.IntegerField(null=False)
+    #total_likes = models.IntegerField(null=False)
     name = models.CharField(max_length=255, null=False)
     email = models.EmailField(max_length=255, null=False)
     password = models.CharField(max_length=255, null=False)
-
-
-    def getUser(self):
-        return self.user_id
 
     def getFollowers(self):
         return self.followers
